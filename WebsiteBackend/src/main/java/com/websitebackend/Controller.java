29f025517f63af55f73913ffd36707d37b7c01fa
Hemant Khaitan <hemant.khaitan@capillarytech.com>
2020-07-10 18:13:51 +0530

    Deleted unnecessary files

--- WebsiteBackend/src/main/java/com/websitebackend/Controller.java
+++
@@ -1,9 +0,0 @@
-package com.websitebackend;
-
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Controller {
-}
